0f1f8906fa13bcb7ff9fa3641db4761c930eeb70
Kashyap2701 <kspatelsimform100@gmail.com>
2024-11-15 14:43:50 +0530

    fix: create index.ts file for all folder

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,7 @@
 import { SchemaCreatorImpl } from './schemaCreator';
-export { SchemaCreatorImpl };
 
-export * from './schemas/arraySchema';
-export * from './schemas/stringSchema';
-export * from './schemas/booleanSchema';
-export * from './schemas/dateSchema';
-export * from './schemas/numberSchema';
-export * from './schemas/objectSchema';
-export * from './schemas/tupleSchema';
-export * from './schemas/unionSchema';
-export * from './schemas/enumSchema';
-export * from './schemas/baseSchema';
-export * from './utils/typeUtils';
-export * from './utils/helper';
-export * from './error/ErrorTypes';
-export * from './error/error';
-export * from './schemaCreator';
+export * from './error';
+export * from './schemas';
+export * from './utils';
 
 export default new SchemaCreatorImpl();
